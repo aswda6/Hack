5682e4fb89385588d6a03d7fec630918ab37ce02
aswda6 <wargame00101@gmail.com>
2025-06-15 21:25:21 +0300

    Create signaling-server.js

---
+++ signaling-server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, { cors: { origin: "*" } });
+
+io.on('connection', socket => {
+  console.log('User connected:', socket.id);
+
+  socket.on('offer', offer => {
+    socket.broadcast.emit('offer', offer);
+  });
+
+  socket.on('answer', answer => {
+    socket.broadcast.emit('answer', answer);
+  });
+
+  socket.on('candidate', candidate => {
+    socket.broadcast.emit('candidate', candidate);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Signaling server running on http://localhost:3000');
+});
